426e01b956a10604a8e1fe30355e80b3af8ae3ea
PPrrooggrraammeerr <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2022-01-23 19:57:23 -0300

    Update Administrator mode DISABLE.ps1

--- Administrator mode DISABLE.ps1
+++ Administrator mode DISABLE.ps1
@@ -30,12 +30,12 @@ Exit
 }
 
 Clear-Host -ErrorAction SilentlyContinue
-$Registry_Path = 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System'
-if (Get-ItemPropertyValue -Path $Registry_Path -Name EnableLUA | Select-String '1' -ErrorAction SilentlyContinue) {Set-ItemProperty -Path $Registry_Path -Name EnableLUA -Value '0' -ErrorAction SilentlyContinue} else {Set-ItemProperty -Path $Registry_Path -Name EnableLUA -Value '0' -ErrorAction SilentlyContinue}
-Start-Sleep -Seconds 2.5 -ErrorAction SilentlyContinue
-$Write_Host = Write-Host;
-Write-Output $Write_Host
-Write-Host EnableLUA changed to 0 -ForegroundColor Cyan -ErrorAction SilentlyContinue
-Start-Sleep -Seconds 2.5 -ErrorAction SilentlyContinue
-Clear-Host -ErrorAction SilentlyContinue
-Exit
+	$Registry_Path = 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System'
+		if (Get-ItemPropertyValue -Path $Registry_Path -Name EnableLUA | Select-String '1' -ErrorAction SilentlyContinue) {Set-ItemProperty -Path $Registry_Path -Name EnableLUA -Value '0' -ErrorAction SilentlyContinue} else {Set-ItemProperty -Path $Registry_Path -Name EnableLUA -Value '0' -ErrorAction SilentlyContinue}
+			Start-Sleep -Seconds 2.5 -ErrorAction SilentlyContinue
+				$Write_Host = Write-Host;
+					Write-Output $Write_Host
+						Write-Host EnableLUA changed to 0 -ForegroundColor Cyan -ErrorAction SilentlyContinue
+							Start-Sleep -Seconds 2.5 -ErrorAction SilentlyContinue
+								Clear-Host -ErrorAction SilentlyContinue
+									Exit
