c970793365f103359c0127313bd2573bc582b6ec
Black coffee <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2024-03-03 01:49:48 -0300

    Add files via upload

---
+++ enable_or_disable_net_adapter.ps1
@@ -0,0 +1,11 @@
+﻿
+### ENABLE OR DISABLE NET ADAPTER.ps1 ###
+
+Clear-Host -ErrorAction SilentlyContinue
+$enable_or_disable_net_adapter = Get-NetAdapter -Name Ethernet; 
+if (
+    $enable_or_disable_net_adapter.Status -eq 'Up') {
+        Stop-Process -Name cmd -Force -Verbose -ErrorAction SilentlyContinue} elseif (
+            $enable_or_disable_net_adapter.Status -eq 'Disabled') {
+                Enable-NetAdapter -Name Ethernet -Verbose -ErrorAction SilentlyContinue
+            }
---
+++ ftp_enable_or_disable.ps1
@@ -0,0 +1,12 @@
+﻿
+### FTP ENABLE OR DISABLE.ps1 ###
+
+Clear-Host -ErrorAction SilentlyContinue
+$ftp_enable_or_disable = Get-Service -Name filezilla-server;
+if (
+    $ftp_enable_or_disable.Status -eq 'Running') {
+        Stop-Process -Name cmd -Force -Verbose -ErrorAction SilentlyContinue} 
+            elseif (
+                $ftp_enable_or_disable.Status -eq 'Stopped') {
+                    Start-Service -Name filezilla-server -Verbose -ErrorAction SilentlyContinue
+                }
