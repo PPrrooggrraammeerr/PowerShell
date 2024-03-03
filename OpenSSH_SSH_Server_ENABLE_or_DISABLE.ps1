bcc5b6df91b2a3bce4abc6ec5bbc4e78a881fc55
Black coffee <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2024-03-03 17:28:36 -0300

    Add files via upload

---
+++ OpenSSH_SSH_Server_ENABLE_or_DISABLE.ps1
@@ -0,0 +1,14 @@
+﻿
+### OpenSSH_SSH_Server_ENABLE_or_DISABLE.ps1 ###
+
+Clear-Host -ErrorAction SilentlyContinue
+$ssh_enable_or_disable = Get-Service -Name sshd; 
+if (
+    $ssh_enable_or_disable.Status -eq 'Running') {
+        Stop-Process -Name cmd -Force -Verbose -ErrorAction SilentlyContinue} 
+            elseif (
+                $ssh_enable_or_disable.Status -eq 'Stopped') {
+                    Start-Service -Name sshd -Verbose -ErrorAction SilentlyContinue
+                }
+
+### -noprofile -executionpolicy bypass -file D:\OpenSSH_SSH_Server_ENABLE_or_DISABLE.ps1 ###
