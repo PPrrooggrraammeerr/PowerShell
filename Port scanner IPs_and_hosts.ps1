205210473cd9a1816c07b2487c7e033f870ba6f6
Black coffee <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2024-04-16 18:52:55 -0300

    Update Port scanner IPs_and_hosts.ps1

--- Port scanner IPs_and_hosts.ps1
+++ Port scanner IPs_and_hosts.ps1
@@ -1,20 +1,32 @@
-﻿
+
 ### Port scanner IPs_and_hosts ###
 
-$File_Path = "$env:USERPROFILE\Desktop\IPs and hosts.txt"
-if (-not (Test-Path -Path $File_Path -PathType Container)) {
+$File_Path_IPs_and_hosts = "$env:USERPROFILE\Desktop\IPs and hosts.txt"
+if (-not (Test-Path -Path $File_Path_IPs_and_hosts)) {
 New-Item -Path "$env:USERPROFILE\Desktop\IPs and hosts.txt" -ItemType File -Force -ErrorAction SilentlyContinue
-} else {Remove-Item -Path "$env:USERPROFILE\Desktop\IPs and hosts.txt" -Force -ErrorAction SilentlyContinue
+} else {
+Remove-Item -Path "$env:USERPROFILE\Desktop\IPs and hosts.txt" -Force -ErrorAction SilentlyContinue
+New-Item -Path "$env:USERPROFILE\Desktop\IPs and hosts.txt" -ItemType File -Force -ErrorAction SilentlyContinue        
 }
 Start-Sleep -Seconds 1 -ErrorAction SilentlyContinue
 Clear-Host -ErrorAction SilentlyContinue
 
-$Out_File_IPs_and_hosts += "ftp.sunet.se`n8.8.8.8`n8.8.4.4`nwww.cloudflare.com`n192.168.0.0`n192.168.0.1`n1.1.1.1`n1.0.0.1`nwww.google.com`nwww.facebook.com`n10.0.0.1"
+$File_Path_Communication_ports = "$env:USERPROFILE\Desktop\Communication ports.txt"
+if (-not (Test-Path -Path $File_Path_Communication_ports)) {
+New-Item -Path "$env:USERPROFILE\Desktop\Communication ports.txt" -ItemType File -Force -ErrorAction SilentlyContinue
+} else {
+Remove-Item -Path "$env:USERPROFILE\Desktop\Communication ports.txt" -Force -ErrorAction SilentlyContinue
+New-Item -Path "$env:USERPROFILE\Desktop\Communication ports.txt" -ItemType File -Force -ErrorAction SilentlyContinue        
+}
+Start-Sleep -Seconds 1 -ErrorAction SilentlyContinue
+Clear-Host -ErrorAction SilentlyContinue
+
+$Out_File_IPs_and_hosts = "ftp.sunet.se`n8.8.8.8`n8.8.4.4`nwww.cloudflare.com`n192.168.0.0`n192.168.0.1`n1.1.1.1`n1.0.0.1`nwww.google.com`nwww.facebook.com`n10.0.0.1"
 Write-Output $Out_File_IPs_and_hosts | Out-File "$env:USERPROFILE\Desktop\IPs and hosts.txt"
 Start-Sleep -Seconds 1 -ErrorAction SilentlyContinue
 Clear-Host -ErrorAction SilentlyContinue
 
-$Out_File_Communication_ports += "21`n22`n23`n25`n53`n80`n123`n179`n443`n500`n3389"
+$Out_File_Communication_ports = "21`n22`n23`n25`n53`n80`n123`n179`n443`n500`n3389"
 Write-Output $Out_File_Communication_ports | Out-File "$env:USERPROFILE\Desktop\Communication ports.txt"
 Start-Sleep -Seconds 1 -ErrorAction SilentlyContinue
 Clear-Host -ErrorAction SilentlyContinue
@@ -29,7 +41,7 @@ Clear-Host -ErrorAction SilentlyContinue
 
 Foreach ($IP_and_host in $IPs_and_hosts) {
 
-	Start-Sleep -Seconds 1 -ErrorAction SilentlyContinue
+    Start-Sleep -Seconds 2.5 -ErrorAction SilentlyContinue
 	Clear-Host -ErrorAction SilentlyContinue
 
 Foreach ($Communication_port in $Communication_ports) {
@@ -57,7 +69,7 @@ Remove-Item -Path "$env:USERPROFILE\Desktop\IPs and hosts.txt" -Force -ErrorActi
 Start-Sleep -Seconds 1 -ErrorAction SilentlyContinue
 Clear-Host -ErrorAction SilentlyContinue
 
-Remove-Item -Path "$env:USERPROFILE\Desktop\IPs and hosts.txt" -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "$env:USERPROFILE\Desktop\Communication ports.txt" -Force -ErrorAction SilentlyContinue
 Start-Sleep -Seconds 1 -ErrorAction SilentlyContinue
 Clear-Host -ErrorAction SilentlyContinue
 
