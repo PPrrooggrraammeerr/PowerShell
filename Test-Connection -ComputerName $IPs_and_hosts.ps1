5ded1f6caa88d4c3e232ccdef79d9c43a232ae26
Black coffee <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2024-04-17 19:52:23 -0300

    Update Test-Connection -ComputerName $IPs_and_hosts.ps1

--- Test-Connection -ComputerName $IPs_and_hosts.ps1
+++ Test-Connection -ComputerName $IPs_and_hosts.ps1
@@ -2,14 +2,16 @@
 ### Test-Connection -ComputerName $IPs_and_hosts ###
 
 $File_Path = "$env:USERPROFILE\Desktop\IPs and hosts.txt"
-if (-not (Test-Path -Path $File_Path -PathType Container)) {
+if (-not (Test-Path -Path $File_Path)) {
 New-Item -Path "$env:USERPROFILE\Desktop\IPs and hosts.txt" -ItemType File -Force -ErrorAction SilentlyContinue
-} else {Remove-Item -Path "$env:USERPROFILE\Desktop\IPs and hosts.txt" -Force -ErrorAction SilentlyContinue
+} else {
+Remove-Item -Path "$env:USERPROFILE\Desktop\IPs and hosts.txt" -Force -ErrorAction SilentlyContinue
+New-Item -Path "$env:USERPROFILE\Desktop\IPs and hosts.txt" -ItemType File -Force -ErrorAction SilentlyContinue
 }
 Start-Sleep -Seconds 1 -ErrorAction SilentlyContinue
 Clear-Host -ErrorAction SilentlyContinue
 
-$Out_File += "ftp.sunet.se`n8.8.8.8`n8.8.4.4`nwww.cloudflare.com`n192.168.0.0`n192.168.0.1`n1.1.1.1`n1.0.0.1`nwww.google.com`nwww.facebook.com`n10.0.0.1"
+$Out_File = "ftp.sunet.se`n8.8.8.8`n8.8.4.4`nwww.cloudflare.com`n192.168.0.0`n192.168.0.1`n1.1.1.1`n1.0.0.1`nwww.google.com`nwww.facebook.com`n10.0.0.1"
 Write-Output $Out_File | Out-File "$env:USERPROFILE\Desktop\IPs and hosts.txt"
 Start-Sleep -Seconds 1 -ErrorAction SilentlyContinue
 Clear-Host -ErrorAction SilentlyContinue
