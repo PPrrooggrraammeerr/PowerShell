b0d4b3706c5d01521ecfded5124a0a4463055844
Black coffee <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2024-04-18 14:39:51 -0300

    Update FileZilla Server ENABLE or DISABLE.ps1

--- FileZilla Server ENABLE or DISABLE.ps1
+++ FileZilla Server ENABLE or DISABLE.ps1
@@ -5,7 +5,7 @@ Clear-Host -ErrorAction SilentlyContinue
 $ftp_enable_or_disable = Get-Service -Name filezilla-server;
 if (
     $ftp_enable_or_disable.Status -eq 'Running') {
-        Stop-Process -Name cmd -Force -Verbose -ErrorAction SilentlyContinue} 
+        Restart-Service -Name filezilla-server -Verbose -ErrorAction SilentlyContinue} 
             elseif (
                 $ftp_enable_or_disable.Status -eq 'Stopped') {
                     Start-Service -Name filezilla-server -Verbose -ErrorAction SilentlyContinue
