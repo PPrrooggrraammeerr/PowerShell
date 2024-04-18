d5618ba0c113caf1c057afea7f26f0cc2a1dc02b
Black coffee <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2024-04-17 22:19:45 -0300

    Add files via upload

---
+++ Administrator mode ENABLE or DESABLE.ps1
@@ -0,0 +1,39 @@
+﻿
+# Administrator mode ENABLE or DISABLE.ps1 #
+
+Clear-Host -ErrorAction SilentlyContinue
+
+$Registry_Path = 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System'
+
+Write-Host `n
+Write-Host " =================================== " -ForegroundColor Cyan
+Write-Host "*                                   *" -ForegroundColor Cyan
+Write-Host "*  1 - Administrator mode ENABLE    *" -ForegroundColor Cyan
+Write-Host "*                                   *" -ForegroundColor Cyan
+Write-Host "*  2 - Administrator mode DISABLE   *" -ForegroundColor Cyan
+Write-Host "*                                   *" -ForegroundColor Cyan
+Write-Host " =================================== " -ForegroundColor Cyan
+Write-Host `n
+
+$choice = Read-Host Type the option
+
+if ($choice -eq '1') {
+    $currentValue = Get-ItemPropertyValue -Path $Registry_Path -Name EnableLUA -ErrorAction SilentlyContinue
+    if ($currentValue -ne '1') {
+        Set-ItemProperty -Path $Registry_Path -Name EnableLUA -Value '1' -ErrorAction SilentlyContinue
+    } else {
+        Exit
+    }
+} elseif ($choice -eq '2') {
+    $currentValue = Get-ItemPropertyValue -Path $Registry_Path -Name EnableLUA -ErrorAction SilentlyContinue
+    if ($currentValue -ne '0') {
+        Set-ItemProperty -Path $Registry_Path -Name EnableLUA -Value '0' -ErrorAction SilentlyContinue
+    } else {
+        Exit
+    }
+} else {
+    Exit
+}
+
+Start-Sleep -Seconds 2.5
+Clear-Host
---
+++ Mouse ENABLE or DESABLE.ps1
@@ -0,0 +1,39 @@
+﻿
+# Mouse ENABLE or DISABLE.ps1 #
+
+Clear-Host -ErrorAction SilentlyContinue
+
+$Registry_Path = 'Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\mouclass'
+
+Write-Host `n
+Write-Host " ====================== " -ForegroundColor Cyan
+Write-Host "*                      *" -ForegroundColor Cyan
+Write-Host "*  1 - Mouse ENABLE    *" -ForegroundColor Cyan
+Write-Host "*                      *" -ForegroundColor Cyan
+Write-Host "*  2 - Mouse DISABLE   *" -ForegroundColor Cyan
+Write-Host "*                      *" -ForegroundColor Cyan
+Write-Host " ====================== " -ForegroundColor Cyan
+Write-Host `n
+
+$choice = Read-Host Type the option
+
+if ($choice -eq '1') {
+    $currentValue = Get-ItemPropertyValue -Path $Registry_Path -Name Start -ErrorAction SilentlyContinue
+    if ($currentValue -ne '3') {
+        Set-ItemProperty -Path $Registry_Path -Name Start -Value '3' -ErrorAction SilentlyContinue
+    } else {
+        Exit
+    }
+} elseif ($choice -eq '2') {
+    $currentValue = Get-ItemPropertyValue -Path $Registry_Path -Name Start -ErrorAction SilentlyContinue
+    if ($currentValue -ne '4') {
+        Set-ItemProperty -Path $Registry_Path -Name Start -Value '4' -ErrorAction SilentlyContinue
+    } else {
+        Exit
+    }
+} else {
+    Exit
+}
+
+Start-Sleep -Seconds 2.5
+Clear-Host
