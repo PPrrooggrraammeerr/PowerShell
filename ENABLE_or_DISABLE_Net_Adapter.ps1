aeb3cd637af8432a4304bbb48eda3421d6de0b5a
Black coffee <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2024-04-18 14:39:13 -0300

    Update ENABLE_or_DISABLE_Net_Adapter.ps1

--- ENABLE_or_DISABLE_Net_Adapter.ps1
+++ ENABLE_or_DISABLE_Net_Adapter.ps1
@@ -5,7 +5,7 @@ Clear-Host -ErrorAction SilentlyContinue
 $enable_or_disable_net_adapter = Get-NetAdapter -Name Ethernet; 
 if (
     $enable_or_disable_net_adapter.Status -eq 'Up') {
-        Stop-Process -Name cmd -Force -Verbose -ErrorAction SilentlyContinue} 
+        Restart-NetAdapter -Name Ethernet -Verbose -ErrorAction SilentlyContinue} 
             elseif (
                 $enable_or_disable_net_adapter.Status -eq 'Disabled') {
                     Enable-NetAdapter -Name Ethernet -Verbose -ErrorAction SilentlyContinue
