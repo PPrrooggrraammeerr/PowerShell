63aa847798d91e364197d5badd1547a4e5351030
Black coffee <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2024-04-18 14:40:18 -0300

    Update OpenSSH_SSH_Server_ENABLE_or_DISABLE.ps1

--- OpenSSH_SSH_Server_ENABLE_or_DISABLE.ps1
+++ OpenSSH_SSH_Server_ENABLE_or_DISABLE.ps1
@@ -5,7 +5,7 @@ Clear-Host -ErrorAction SilentlyContinue
 $ssh_enable_or_disable = Get-Service -Name sshd; 
 if (
     $ssh_enable_or_disable.Status -eq 'Running') {
-        Stop-Process -Name cmd -Force -Verbose -ErrorAction SilentlyContinue} 
+        Restart-Service -Name sshd -Verbose -ErrorAction SilentlyContinue} 
             elseif (
                 $ssh_enable_or_disable.Status -eq 'Stopped') {
                     Start-Service -Name sshd -Verbose -ErrorAction SilentlyContinue
