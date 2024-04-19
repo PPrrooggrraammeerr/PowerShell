f677dff74f30a15728a7af0ce041db66b3313f87
Black coffee <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2024-04-19 17:59:31 -0300

    Add files via upload

---
+++ Start_or_Restart_WlanSvc.ps1
@@ -0,0 +1,14 @@
+﻿
+### Start_or_Restart_WlanSvc.ps1 ###
+
+Clear-Host -ErrorAction SilentlyContinue
+$start_or_restart_wlansvc = Get-Service -Name WlanSvc; 
+if (
+    $start_or_restart_wlansvc.Status -eq 'Running') {
+        Restart-Service -Name WlanSvc -Verbose -ErrorAction SilentlyContinue} 
+            elseif (
+                $start_or_restart_wlansvc.Status -eq 'Stopped') {
+                    Start-Service -Name WlanSvc -Verbose -ErrorAction SilentlyContinue
+                }
+
+### -noprofile -executionpolicy bypass -file D:\Start_or_Restart_WlanSvc.ps1 ###
