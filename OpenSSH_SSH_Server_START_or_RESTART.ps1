e5c4b1c07e9c4c620934d3281c71bec027660d78
Black coffee <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2024-04-19 17:55:25 -0300

    Update and rename OpenSSH_SSH_Server_ENABLE_or_DISABLE.ps1 to OpenSSH_SSH_Server_START_or_RESTART.ps1

---
+++ OpenSSH_SSH_Server_START_or_RESTART.ps1
@@ -0,0 +1,14 @@
+﻿
+### OpenSSH_SSH_Server_START_or_RESTART.ps1 ###
+
+Clear-Host -ErrorAction SilentlyContinue
+$ssh_start_or_restart = Get-Service -Name sshd; 
+if (
+    $ssh_start_or_restart.Status -eq 'Running') {
+        Restart-Service -Name sshd -Verbose -ErrorAction SilentlyContinue} 
+            elseif (
+                $ssh_start_or_restart.Status -eq 'Stopped') {
+                    Start-Service -Name sshd -Verbose -ErrorAction SilentlyContinue
+                }
+
+### -noprofile -executionpolicy bypass -file D:\OpenSSH_SSH_Server_START_or_RESTART.ps1 ###
