d1107d7e37dd75b769a7afbc88e73abd25f09046
Black coffee <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2024-04-19 17:56:14 -0300

    Update and rename FileZilla Server ENABLE or RESTART.ps1 to FileZilla Server START or RESTART.ps1

---
+++ FileZilla Server START or RESTART.ps1
@@ -0,0 +1,14 @@
+﻿
+### FileZilla Server START or RESTART.ps1 ###
+
+Clear-Host -ErrorAction SilentlyContinue
+$ftp_start_or_restart = Get-Service -Name filezilla-server;
+if (
+    $ftp_start_or_restart.Status -eq 'Running') {
+        Restart-Service -Name filezilla-server -Verbose -ErrorAction SilentlyContinue} 
+            elseif (
+                $ftp_start_or_restart.Status -eq 'Stopped') {
+                    Start-Service -Name filezilla-server -Verbose -ErrorAction SilentlyContinue
+                }
+
+### -noprofile -executionpolicy bypass -file D:\FileZilla Server START or RESTART.ps1 ###
