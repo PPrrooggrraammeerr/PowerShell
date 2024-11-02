61cdb3aa7b0377809de3e083f878733ca4c39f9c
Black coffee <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2024-11-02 17:29:46 -0300

    Add files via upload

---
+++ IPv4_Gateway_and_DNS_defined_for_adapter_Wi-Fi.ps1
@@ -0,0 +1,30 @@
+﻿
+### IPv4, Gateway and DNS defined for adapter Wi-Fi ###
+
+$wifiInfo = Get-NetIPAddress | Where-Object { $_.InterfaceAlias -eq 'Wi-Fi' }
+
+$dhcpConfig = $wifiInfo | Where-Object {
+
+$_.PrefixOrigin -eq 'Dhcp' -and
+$_.SuffixOrigin -eq 'Dhcp' -and
+$_.PrefixLength -eq 24
+
+}
+
+if ($dhcpConfig) {
+
+$IPv4 = Read-Host "IPv4"
+$Gateway = Read-Host "Gateway"
+$DNS = Read-Host "DNS"
+
+New-NetIPAddress -IPAddress $IPv4 -InterfaceAlias "Wi-Fi" -DefaultGateway $Gateway -AddressFamily IPv4 -PrefixLength 24
+Set-DnsClientServerAddress -InterfaceAlias "Wi-Fi" -ServerAddresses $DNS
+
+} else {
+
+Set-NetIPInterface -InterfaceAlias "Wi-Fi" -DHCP enabled
+Set-DnsClientServerAddress -InterfaceAlias "Wi-Fi" -ResetServerAddress
+Remove-NetRoute -InterfaceAlias "Wi-Fi"
+Restart-NetAdapter -InterfaceAlias "Wi-Fi"
+
+}
