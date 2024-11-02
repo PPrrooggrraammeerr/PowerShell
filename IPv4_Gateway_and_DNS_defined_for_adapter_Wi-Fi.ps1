a591fa8ea8821e398233360d31331cd35a35019d
Black coffee <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2024-11-02 18:03:17 -0300

    Update IPv4_Gateway_and_DNS_defined_for_adapter_Wi-Fi.ps1

--- IPv4_Gateway_and_DNS_defined_for_adapter_Wi-Fi.ps1
+++ IPv4_Gateway_and_DNS_defined_for_adapter_Wi-Fi.ps1
@@ -17,7 +17,7 @@ $IPv4 = Read-Host "IPv4"
 $Gateway = Read-Host "Gateway"
 $DNS = Read-Host "DNS"
 
-DNSArray = $DNS -split ','
+$DNSArray = $DNS -split ','
 
 New-NetIPAddress -IPAddress $IPv4 -InterfaceAlias "Wi-Fi" -DefaultGateway $Gateway -AddressFamily IPv4 -PrefixLength 24
 Set-DnsClientServerAddress -InterfaceAlias "Wi-Fi" -ServerAddresses $DNSArray
