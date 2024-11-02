f4d7641ddb72b4c07f1a49437757eed5738408a6
Black coffee <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2024-11-02 18:18:53 -0300

    Update IPv4_Gateway_and_DNS_defined_for_adapter_Wi-Fi.ps1

--- IPv4_Gateway_and_DNS_defined_for_adapter_Wi-Fi.ps1
+++ IPv4_Gateway_and_DNS_defined_for_adapter_Wi-Fi.ps1
@@ -1,4 +1,4 @@
-﻿
+
 ### IPv4, Gateway and DNS defined for adapter Wi-Fi ###
 
 $wifiInfo = Get-NetIPAddress | Where-Object { $_.InterfaceAlias -eq 'Wi-Fi' }
@@ -15,17 +15,17 @@ if ($dhcpConfig) {
 
 $IPv4 = Read-Host "IPv4"
 $Gateway = Read-Host "Gateway"
-$DNS = Read-Host "DNS"
-
-$DNSArray = $DNS -split ','
+$DNS_preferential = Read-Host "DNS preferential"
+$DNS_alternative = Read-Host "DNS alternative"
 
 New-NetIPAddress -IPAddress $IPv4 -InterfaceAlias "Wi-Fi" -DefaultGateway $Gateway -AddressFamily IPv4 -PrefixLength 24
-Set-DnsClientServerAddress -InterfaceAlias "Wi-Fi" -ServerAddresses $DNSArray
+Set-DnsClientServerAddress -InterfaceAlias "Wi-Fi" -ResetServerAddresses
+Set-DnsClientServerAddress -InterfaceAlias "Wi-Fi" -ServerAddresses @($DNS_preferential, $DNS_alternative)
 
 } else {
 
 Set-NetIPInterface -InterfaceAlias "Wi-Fi" -DHCP enabled
-Set-DnsClientServerAddress -InterfaceAlias "Wi-Fi" -ResetServerAddress
+Set-DnsClientServerAddress -InterfaceAlias "Wi-Fi" -ResetServerAddresses
 Remove-NetRoute -InterfaceAlias "Wi-Fi"
 Restart-NetAdapter -InterfaceAlias "Wi-Fi"
 
